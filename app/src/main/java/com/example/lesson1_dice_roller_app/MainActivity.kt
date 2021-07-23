e7f290b6a4e75627f6e43fb2c92d5c836b74f5e9
emdadul_islam <emdadulislam439@gmail.com>
2021-07-23 06:59:32 +0600

    made some modification...

--- app/src/main/java/com/example/lesson1_dice_roller_app/MainActivity.kt
+++ app/src/main/java/com/example/lesson1_dice_roller_app/MainActivity.kt
@@ -19,8 +19,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollTheDice() {
         Toast.makeText(this, "Button Pressed", Toast.LENGTH_SHORT).show()
-        val diceNumberToBeRolled = Random().nextInt(6)+1
-        val drawableResource = when (diceNumberToBeRolled) {
+        val drawableResource = when (Random().nextInt(6)+1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
